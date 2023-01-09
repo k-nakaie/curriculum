a7dc76ec71b7b69c0df16ad90f5212e4759ffcfd
k-nakaie <102917326+k-nakaie@users.noreply.github.com>
2023-01-09 22:15:01 +0900

    Revert "５章課題"
    This reverts commit ae6fb90cd9ae298af12b2bdbb932edcb2d383e0e.

--- JavaStudy/2-2/src/study/Main.java
+++ JavaStudy/2-2/src/study/Main.java
@@ -1,59 +1,32 @@
 package study;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
- * 本問題では、コレクションフレームワークのList系クラスとMap系クラスの基本的な記述を学びましょう。
- * 現場で必ず使用するものなので、ポイントをしっかり押さえておきましょう。
+ * Main.java
  *
- * 問①〜問③まであります。
- * 問②に関しては、コメントを記述してください。
- *
+ * 本課題では、mainメソッド・インスタンス・メソッドの呼び出しの使い方を学んでいきましょう。
+ * 課題範囲は以下のとおりです。
+ *   User.java: 問①
+ *   Main.java: 問①から問②
+ * 指定された値と変数名を守って記述してください。
  */
 public class Main {
 
-    public static final String SHOP_SHOHIN_00 = "バナナ";
-    public static final String SHOP_SHOHIN_01 = "牛乳";
-    public static final String SHOP_SHOHIN_02 = "豚肉";
-    public static final String SHOP_SHOHIN_03 = "コロッケ";
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+        // 変数名:「user」
+		User user = new User("hoge",123,"abc123");
 
-    public static void main(String args[]) {
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+		
+		user.printAccountInfo();
+			
+		
+		
+		
+		
+		
 
-        // ① 定数を全て使って、String型のListを記述してください。
-    	List<String> shohinList = new ArrayList<>();
-    	shohinList.add(SHOP_SHOHIN_00);
-    	shohinList.add(SHOP_SHOHIN_01);
-    	shohinList.add(SHOP_SHOHIN_02);
-    	shohinList.add(SHOP_SHOHIN_03);
+	}
 
-
-
-
-
-
-        // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
-        /*
-         * [ここへ記述]商品リストの二番目の要素をキー、180を値にした挿入順のマップを作成している
-         *
-         */
-        LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
-        shopMap.put(shohinList.get(0), 125);
-        shopMap.put(shohinList.get(1), 180);
-        shopMap.put(shohinList.get(2), 350);
-        shopMap.put(shohinList.get(3), 100);
-
-        // ③ カリキュラムを参考に拡張for文を使って、問題の画像と同じ表示になるよう記述してください。
-        // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
-        for(Map.Entry<String, Integer> entry : shopMap.entrySet()) {
-        	//System.out.println(entry.getKey() + "=" + entry.getValue() + "円になります！");
-        	System.out.println(entry.toString() + "円になります！");
-        }
-
-
-
-
-    }
 }
---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,26 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+
+	private static String firstName = "中家";
+	private static String lastName = "健吾";
+
+	private static void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド → " + firstName + lastName);
+
+	}
+
+	public static void main(String[] args) {
+		printName(firstName, lastName);
+
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+
+	}
+
+}
