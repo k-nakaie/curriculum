ae6fb90cd9ae298af12b2bdbb932edcb2d383e0e
k-nakaie <102917326+k-nakaie@users.noreply.github.com>
2023-01-09 22:12:34 +0900

    ５章課題
    5-1
    Main.java

---
+++ JavaStudy/2-2/src/study3/Main.java
@@ -0,0 +1,20 @@
+package study3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy");
+		Date date = new Date();
+		
+		System.out.println(sdf1.format(date));
+		System.out.println(sdf2.format(date));
+		System.out.printf("%tY年%<tB%<te日(%<ta)%<tp%<tI時%<tM分%<tS秒", date);
+	}
+
+}
